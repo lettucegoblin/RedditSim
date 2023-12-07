bace28e26aaa1de98dbf019af1700834af94352c
lettucegoblin <77356082+lettucegoblin@users.noreply.github.com>
2023-12-07 01:28:17 -0500

    commentPaths have loading

--- client/src/components/PostModal.vue
+++ client/src/components/PostModal.vue
@@ -8,8 +8,7 @@ import {
   queueCommentPath,
   getQueueCommentPathStatus
 } from '@/model/queueInference'
-import type {  DataListEnvelope } from '@/model/myFetch'
-
+import type { DataListEnvelope } from '@/model/myFetch'
 
 const props = defineProps({
   post: {
@@ -47,7 +46,7 @@ const generateComment = () => {
 
   const commentPath = {
     commentPath: [],
-    postObj: { ...post.value, image : undefined},
+    postObj: { ...post.value, image: undefined },
     numberOfComments: Math.floor(Math.random() * 4) + 1 // 1-4
     //nextUser: 'lettuce'
   } as CommentPathPending
@@ -63,20 +62,20 @@ const generateComment = () => {
 const updateCommentPathStatus = () => {
   console.log('updateCommentPathStatus')
   pendingPathsStatus.value.forEach((commentPathStatus: any) => {
-    
     getQueueCommentPathStatus(commentPathStatus.queueId).then(
       (commentPathStatus: CommentPathStatus) => {
         console.log('commentPathStatus', commentPathStatus)
         if (commentPathStatus.message === 'done') {
+          pendingPathsStatus.value = pendingPathsStatus.value.filter(
+            (commentPathStatus: CommentPathStatus) => commentPathStatus.queueId !== commentPathStatus.queueId
+          )
           getCommentPathsBySubmissionId(String(post.value._id)).then(
             (envelope: DataListEnvelope<CommentPath>) => {
               console.log('commentPaths', envelope.data)
               completedPaths.value = envelope.data
             }
           )
-        }
-        else
-          setTimeout(updateCommentPathStatus, 1000)
+        } else setTimeout(updateCommentPathStatus, 1000)
       }
     )
   })
@@ -84,14 +83,9 @@ const updateCommentPathStatus = () => {
 </script>
 
 <template>
-  <div
-    id="SubmissionModalWrapper"
-    class="absolute top-0 left-0 w-full h-full flex items-center justify-center"
-  >
-    <div
-      id="hideSubmissionModal"
-      class="rounded-xl bg-gray-100 dark:bg-reddit-neutral-background-dark fixed z-50 w-11/12 overflow-y-scroll h-5/6"
-    >
+  <div id="SubmissionModalWrapper" class="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+    <div id="hideSubmissionModal"
+      class="rounded-xl bg-gray-100 dark:bg-reddit-neutral-background-dark fixed z-50 w-11/12 overflow-y-scroll h-5/6">
       <div class="rounded-lg p-6">
         <h2>{{ post.title }}</h2>
         <p>{{ post.author }}</p>
@@ -102,11 +96,7 @@ const updateCommentPathStatus = () => {
         <button @click="generateComment" class="bg-reddit-blue text-white rounded-lg px-4 py-2 m-2">
           Generate Comment Path
         </button>
-        <div
-          class="comments rounded-lg"
-          v-for="commentPath in completedPaths"
-          :key="commentPath._id"
-        >
+        <div class="comments rounded-lg" v-for="commentPath in completedPaths" :key="commentPath._id">
           <div v-for="(comment, index) in commentPath.commentPath" :key="comment._id">
             <div class="comment" :style="{ marginLeft: `${Number(index) * 20}px` }">
               <h1>{{ comment.user }}</h1>
@@ -114,13 +104,17 @@ const updateCommentPathStatus = () => {
             </div>
           </div>
         </div>
+        <!-- pendingPathsStatus -->
+        <div v-for="commentPathStatus in pendingPathsStatus" :key="commentPathStatus.queueId">
+          <div class="flex align-middle justify-center mb-3 mt-5">
+            <h2>Generating comment path of {{ commentPathStatus.numberOfComments }} comments... queue:{{ commentPathStatus.queueIndex }}/{{
+              commentPathStatus.queueLength }}</h2>
+            <div class="grayscale dot-bricks ml-5 "></div>
+          </div>
+        </div>
       </div>
     </div>
-    <div
-      id="hideSubmissionModal_blackout"
-      @click="hideSubmissionModal"
-      class="fixed inset-0 bg-black opacity-25"
-    ></div>
+    <div id="hideSubmissionModal_blackout" @click="hideSubmissionModal" class="fixed inset-0 bg-black opacity-25"></div>
   </div>
 </template>
 
